dd401f9f1ce3476669369ec1b94775e71842297c
Benjamin Serrano <b.serrano8970@gmail.com>
2021-11-03 22:28:56 -0500

    Using only public function

--- Classes/ClassWithMemberFunction.cc
+++ Classes/ClassWithMemberFunction.cc
@@ -1,5 +1,6 @@
 //Program to demonstrate a very simple example of a class.
 //A better version will be displayed later
+//Taken from page 555 of Savitch, 9th edition
 #include<iostream>
 using namespace std;
 
@@ -17,4 +18,26 @@ int main(void){
   cin >> today.month;
   cout << "Enter the day of the month: ";
   cin >> today.day;
+  cout << "Enter your birthday:\n";
+  cout << "Enter the month as a number: ";
+  cin >> birthday.month;
+  cout << "Enter the day of the month: ";
+  cin >> birthday.day;
+
+  cout << "Today's date is ";
+  today.output();
+  cout << "Your birthday is ";
+  birthday.output();
+
+  if(today.month == birthday.month && today.day == birthday.day){
+    cout << "Happy Birthday!\n";
+  }else{
+    cout << "Happy Unbirthday!\n";
+  }
+  return 0;
+}
+
+void DayOfYear::output(void){
+  cout << "month = " << month;
+  cout << ", day = " << day << endl;
 }
